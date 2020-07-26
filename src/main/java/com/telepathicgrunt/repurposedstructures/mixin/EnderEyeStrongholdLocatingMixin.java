d309d45113bde7630cff657a2ae262c90a43b249
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2020-07-26 16:00:26 -0400

    Update EnderEyeStrongholdLocatingMixin.java

--- src/main/java/com/telepathicgrunt/repurposedstructures/mixin/EnderEyeStrongholdLocatingMixin.java
+++ src/main/java/com/telepathicgrunt/repurposedstructures/mixin/EnderEyeStrongholdLocatingMixin.java
@@ -34,12 +34,19 @@ public class EnderEyeStrongholdLocatingMixin {
         else if(blockPos1 != null && blockPos2 == null){
             return blockPos1;
         }
-        else if(blockPos1 != null && blockPos2 != null &&
-                Math.pow(blockPos1.getX() - centerPos.getX(), 2) + Math.pow(blockPos1.getZ() - centerPos.getZ(), 2) >
-                        Math.pow(blockPos2.getX() - centerPos.getX(), 2) + Math.pow(blockPos2.getZ() - centerPos.getZ(), 2)){
-            return blockPos2;
+        else if(blockPos1 == null && blockPos2 == null){
+            return null;
+        }
+        else{
+            double distance1 = Math.pow(blockPos1.getX() - centerPos.getX(), 2) + Math.pow(blockPos1.getZ() - centerPos.getZ(), 2);
+            double distance2 = Math.pow(blockPos2.getX() - centerPos.getX(), 2) + Math.pow(blockPos2.getZ() - centerPos.getZ(), 2);
+            if(distance1 < distance2){
+                return blockPos1;
+            }
+            else{
+                return blockPos2;
+            }
         }
 
-        return null;
     }
 }
