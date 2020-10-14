bad7ddb4671ec517131bb646e01f2a2c0bde48aa
Alex Sarnese <40846040+TelepathicGrunt@users.noreply.github.com>
2020-10-14 18:19:07 -0400

    cleaned up eye mixin and updated pack num

--- src/main/java/com/telepathicgrunt/repurposedstructures/mixin/EnderEyeStrongholdLocatingMixin.java
+++ src/main/java/com/telepathicgrunt/repurposedstructures/mixin/EnderEyeStrongholdLocatingMixin.java
@@ -6,6 +6,7 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.EnderEyeItem;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.gen.ChunkGenerator;
 import net.minecraft.world.server.ServerWorld;
 import org.apache.logging.log4j.Level;
 import org.spongepowered.asm.mixin.Mixin;
@@ -20,31 +21,34 @@ public class EnderEyeStrongholdLocatingMixin {
             at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/world/gen/ChunkGenerator;locateStructure(Lnet/minecraft/world/server/ServerWorld;Lnet/minecraft/world/gen/feature/structure/Structure;Lnet/minecraft/util/math/BlockPos;IZ)Lnet/minecraft/util/math/BlockPos;")
     )
     private BlockPos locateRSStrongholds(BlockPos blockPos, World world, PlayerEntity user) {
-        RepurposedStructures.LOGGER.log(Level.INFO, "yip yip finding stronghold now");
-        return returnClosestStronghold(blockPos, world, user);
+        //RepurposedStructures.LOGGER.log(Level.INFO, "yip yip finding stronghold now");
+        return returnClosestStronghold(blockPos, (ServerWorld)world, user.getBlockPos());
     }
 
-    private static BlockPos returnClosestStronghold(BlockPos blockPos, World world, PlayerEntity user) {
-        BlockPos closestPos = returnCloserPos(blockPos, ((ServerWorld) world).getChunkProvider().getChunkGenerator().locateStructure((ServerWorld) world, RSStructures.STONEBRICK_STRONGHOLD, user.getBlockPos(), 100, false), user.getBlockPos());
-        closestPos = returnCloserPos(closestPos, ((ServerWorld) world).getChunkProvider().getChunkGenerator().locateStructure((ServerWorld) world, RSStructures.NETHER_STRONGHOLD, user.getBlockPos(), 100, false), user.getBlockPos());
+    private static BlockPos returnClosestStronghold(BlockPos blockPos, ServerWorld world, BlockPos playerPos) {
+        ChunkGenerator chunkGenerator = world.getChunkProvider().getChunkGenerator();
+        BlockPos closestPos = returnCloserPos(blockPos, chunkGenerator.locateStructure(world, RSStructures.STONEBRICK_STRONGHOLD, playerPos, 100, false), playerPos);
+        closestPos = returnCloserPos(closestPos, chunkGenerator.locateStructure(world, RSStructures.NETHER_STRONGHOLD, playerPos, 100, false), playerPos);
         return closestPos;
     }
 
     private static BlockPos returnCloserPos(BlockPos blockPos1, BlockPos blockPos2, BlockPos centerPos) {
-        if (blockPos1 == null && blockPos2 != null) {
-            return blockPos2;
-        } else if (blockPos1 != null && blockPos2 == null) {
-            return blockPos1;
-        } else if (blockPos1 == null && blockPos2 == null) {
+        if (blockPos1 == null && blockPos2 == null) {
             return null;
+        }
+        else if (blockPos1 == null) {
+            return blockPos2;
+        }
+        else if (blockPos2 == null) {
+            return blockPos1;
+        }
+
+        double distance1 = Math.pow(blockPos1.getX() - centerPos.getX(), 2) + Math.pow(blockPos1.getZ() - centerPos.getZ(), 2);
+        double distance2 = Math.pow(blockPos2.getX() - centerPos.getX(), 2) + Math.pow(blockPos2.getZ() - centerPos.getZ(), 2);
+        if (distance1 < distance2) {
+            return blockPos1;
         } else {
-            double distance1 = Math.pow(blockPos1.getX() - centerPos.getX(), 2) + Math.pow(blockPos1.getZ() - centerPos.getZ(), 2);
-            double distance2 = Math.pow(blockPos2.getX() - centerPos.getX(), 2) + Math.pow(blockPos2.getZ() - centerPos.getZ(), 2);
-            if (distance1 < distance2) {
-                return blockPos1;
-            } else {
-                return blockPos2;
-            }
+            return blockPos2;
         }
     }
 }
