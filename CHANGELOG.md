31fc370cc693caf4eeb50cede09ebfa5b6d70dca
Alex Sarnese <40846040+TelepathicGrunt@users.noreply.github.com>
2021-02-21 00:04:41 -0500

    fixed stone shores still having vanilla stronghold in it
    https://github.com/TelepathicGrunt/RepurposedStructures-Fabric/issues/73

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -16,6 +16,8 @@ Welcome to the Github! If you are looking for the most recent stable version, th
 ##### Strongholds:
 - Added turnOffVanillaStrongholds config option to let people be able to turn off vanilla Strongholds to only have Nether Strongholds easier.
 
+- Fixed Stone Shores still having Vanilla Strongholds instead of Repurposed Structures which was causing false positives with /locate stronghold and Eyes of Ender as a result.
+
 ##### General:
 - Made all the checks I have for separating some structures from other kinds of structures now ignore those checks if the structure spacing is set super low in configs.
   Now if you make all outposts spawn in every chunk by config, Repurposed Structures villages will still be able to spawn.
--- src/main/java/com/telepathicgrunt/repurposedstructures/RSAddFeaturesAndStructures.java
+++ src/main/java/com/telepathicgrunt/repurposedstructures/RSAddFeaturesAndStructures.java
@@ -377,7 +377,8 @@ public class RSAddFeaturesAndStructures {
 
     public static void addStrongholds() {
         addToBiome("stonebrick_stronghold",
-                (context) -> !BiomeSelection.haveCategories(context, Category.NETHER, Category.THEEND, Category.NONE)
+                (context) -> !BiomeSelection.haveCategories(context, Category.NETHER, Category.THEEND) &&
+                        (!BiomeSelection.haveCategories(context, Category.NONE) && !context.getBiomeKey().equals(BiomeKeys.STONE_SHORE))
                         && RepurposedStructures.RSAllConfig.RSStrongholdsConfig.stonebrick.stonebrickStrongholdMaxChunkDistance != 1001
                         && BiomeSelection.doesNotHaveStructureType(context, RSStructureTagMap.STRUCTURE_TAGS.STRONGHOLD)
                         && BiomeSelection.isBiomeAllowed(context, "strongholds")
