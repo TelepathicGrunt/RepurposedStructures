370146d04db11345bff031f3edefdb190bc23f03
Alex Sarnese <40846040+TelepathicGrunt@users.noreply.github.com>
2021-03-09 15:56:50 -0500

    fixed translations to use english text as translation keys

--- script/structure_creation/advancements/Repurposed_Structures-Translation_Resourcepack/fix_translation.py
+++ script/structure_creation/advancements/Repurposed_Structures-Translation_Resourcepack/fix_translation.py
@@ -6,19 +6,21 @@ import sys
 import random
 import re
 
-translations = ""
+translations = []
 with open(os.path.join('assets', 'repurposed_structures', 'lang', 'en_us.json'), "r") as file:
         translations = json.load(file)
         
 directory = os.path.dirname(os.path.dirname(__file__))
-for entry in os.scandir(directory):
-    if (entry.path.endswith(".json") and entry.is_file()):
-        newText = ""
-        with open(entry) as f:
-            fileText = f.read()
-            for key in translations.keys():
-                newText = fileText.replace(key, translations[key])
+for subdir, dirs, files in os.walk(directory):
+    for entry in files:
+        if entry.endswith(".json"):
+            newText = ""
+            with open(os.path.join(subdir, entry), encoding="utf8") as f:
+                newText = f.read()
+                for key in translations.keys():
+                    if "advancements" in key:
+                        newText = newText.replace(key, translations[key])
 
-        with open(entry, "w") as f:
-            f.write(newText)
+            with open(os.path.join(subdir, entry), "w", encoding="utf8") as f:
+                f.write(newText)
     
