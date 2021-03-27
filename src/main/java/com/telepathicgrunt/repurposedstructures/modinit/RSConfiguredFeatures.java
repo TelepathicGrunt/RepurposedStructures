8d822966269db3f03c8fa7a7a6bd870686c45d7b
Alex Sarnese <40846040+TelepathicGrunt@users.noreply.github.com>
2021-03-26 22:20:09 -0400

    reduced max air space for dungeons back to 14

--- src/main/java/com/telepathicgrunt/repurposedstructures/modinit/RSConfiguredFeatures.java
+++ src/main/java/com/telepathicgrunt/repurposedstructures/modinit/RSConfiguredFeatures.java
@@ -40,7 +40,7 @@ public class RSConfiguredFeatures {
 
     private static NbtDungeonConfig getNbtDungeonConfig(String dungeonType, String spawnerType){
         return new NbtDungeonConfig(
-                false,1, 16, 2,
+                false,1, 14, 2,
                 false, Optional.empty(), 0, Blocks.CHEST.getDefaultState(),
                 new Identifier(RepurposedStructures.MODID, "chests/dungeon/"+dungeonType),
                 new Identifier(RepurposedStructures.MODID, "dungeon_"+spawnerType),
