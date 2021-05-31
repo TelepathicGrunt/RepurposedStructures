fa2548350583ab0b1add0c8e9526c8717f04410a
Alex Sarnese <40846040+TelepathicGrunt@users.noreply.github.com>
2021-05-31 12:14:53 -0400

    renamed 2 mixins

--- src/main/java/com/telepathicgrunt/repurposedstructures/mixin/NoLavaFallsInStructuresMixin.java
+++ src/main/java/com/telepathicgrunt/repurposedstructures/mixin/NoLavaFallsInStructuresMixin.java
@@ -26,7 +26,7 @@ public class NoLavaFallsInStructuresMixin {
             at = @At(value = "HEAD"),
             cancellable = true
     )
-    private void repurposedstructures_noLava(ISeedReader structureWorldAccess, ChunkGenerator chunkGenerator, Random random, BlockPos blockPos, LiquidsConfig springFeatureConfig, CallbackInfoReturnable<Boolean> cir) {
+    private void repurposedstructures_noLavaInStructures(ISeedReader structureWorldAccess, ChunkGenerator chunkGenerator, Random random, BlockPos blockPos, LiquidsConfig springFeatureConfig, CallbackInfoReturnable<Boolean> cir) {
         if(springFeatureConfig.state.is(FluidTags.LAVA)) {
             BlockPos.Mutable mutable = new BlockPos.Mutable();
             SectionPos sectionPos;
