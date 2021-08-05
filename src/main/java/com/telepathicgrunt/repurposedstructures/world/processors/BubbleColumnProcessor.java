c4be384dd4f71380279d95e06eff2ea33b633a65
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2021-08-05 17:33:47 -0400

    clean up bubble column code much more

--- src/main/java/com/telepathicgrunt/repurposedstructures/world/processors/BubbleColumnProcessor.java
+++ src/main/java/com/telepathicgrunt/repurposedstructures/world/processors/BubbleColumnProcessor.java
@@ -6,6 +6,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.chunk.ChunkAccess;
+import net.minecraft.world.level.chunk.LevelChunkSection;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructurePlaceSettings;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProcessor;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProcessorType;
@@ -25,14 +26,8 @@ public class BubbleColumnProcessor extends StructureProcessor {
         if(structureBlockInfoWorld.state.getBlock() == Blocks.BUBBLE_COLUMN){
             ChunkAccess chunk = worldView.getChunk(structureBlockInfoWorld.pos);
 
-            // We use chunk sections instead of the world's min and max y value because a structure piece can generate
-            // right on the edge of the lowest or highest chunk section and cut into a non-existent section.
-            // Checking the world's min and max y would prevent the fluid tick crashing when assigned a position in a
-            // non-existent chunk section but it may still crash Cubic Chunks or cause weird behaviors with it.
-            // Using chunk sections will help make sure this processor works without crashing Cubic Chunks hopefully lol.
-            if(chunk.getSections().length != 0 &&
-                chunk.getSections()[0].bottomBlockY() > structureBlockInfoWorld.pos.getY() &&
-                chunk.getSections()[chunk.getSections().length - 1].bottomBlockY() + 15 < structureBlockInfoWorld.pos.getY())
+            if(chunk.getMinBuildHeight() >= structureBlockInfoWorld.pos.getY() &&
+                chunk.getMaxBuildHeight() <= structureBlockInfoWorld.pos.getY())
             {
                 chunk.getBlockTicks().scheduleTick(structureBlockInfoWorld.pos,  structureBlockInfoWorld.state.getBlock(), 0);
             }
