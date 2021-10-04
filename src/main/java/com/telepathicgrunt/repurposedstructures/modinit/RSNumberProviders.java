d677ceb5648a139c7125d2312825804b9de3fa73
TelepathicGrunt <alexsarnese@gmail.com>
2021-10-04 15:15:51 -0400

    optimized imports

--- src/main/java/com/telepathicgrunt/repurposedstructures/modinit/RSNumberProviders.java
+++ src/main/java/com/telepathicgrunt/repurposedstructures/modinit/RSNumberProviders.java
@@ -2,13 +2,8 @@ package com.telepathicgrunt.repurposedstructures.modinit;
 
 import com.telepathicgrunt.repurposedstructures.RepurposedStructures;
 import com.telepathicgrunt.repurposedstructures.world.numberproviders.ProbabilityValue;
-import com.telepathicgrunt.repurposedstructures.world.predicates.MatterPhaseRuleTest;
-import com.telepathicgrunt.repurposedstructures.world.predicates.PieceOriginAxisAlignedLinearPosRuleTest;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.levelgen.structure.templatesystem.PosRuleTestType;
-import net.minecraft.world.level.levelgen.structure.templatesystem.RuleTestType;
-import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
 import net.minecraft.world.level.storage.loot.providers.number.LootNumberProviderType;
 
 public class RSNumberProviders {
