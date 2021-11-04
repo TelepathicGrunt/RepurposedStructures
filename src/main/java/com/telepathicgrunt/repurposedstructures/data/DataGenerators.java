c8aede81733a003a3a07814de4d6fc878c86554f
TelepathicGrunt <alexsarnese@gmail.com>
2021-11-04 17:01:58 -0400

    make sure datagen cannot run outside rs dev

--- src/main/java/com/telepathicgrunt/repurposedstructures/data/DataGenerators.java
+++ src/main/java/com/telepathicgrunt/repurposedstructures/data/DataGenerators.java
@@ -1,10 +1,19 @@
 package com.telepathicgrunt.repurposedstructures.data;
 
+import com.telepathicgrunt.repurposedstructures.RepurposedStructures;
 import net.minecraft.data.DataGenerator;
 import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
 
-public class DataGenerators {
+public final class DataGenerators {
+    // Set to true when doing RS genData.
     public static boolean datagenLootTableModeOn = false;
+    static {
+        // Makes sure that RS's datagen mixins and datagen methods does not run outside RS's dev environment ever.
+        if(RepurposedStructures.class.getPackage().getImplementationVersion() != null)
+            datagenLootTableModeOn = false;
+    }
+
+    private DataGenerators() {}
 
     public static void gatherData(GatherDataEvent evt) {
         DataGenerator generator = evt.getGenerator();
