8ba167dbf72df3cfb1fb6875536f37d4adb06188
TelepathicGrunt <alexsarnese@gmail.com>
2022-04-02 10:17:59 -0400

    finalized monuments

--- src/main/java/com/telepathicgrunt/repurposedstructures/world/structures/configs/RSMonumentConfig.java
+++ src/main/java/com/telepathicgrunt/repurposedstructures/world/structures/configs/RSMonumentConfig.java
@@ -19,25 +19,29 @@ public class RSMonumentConfig implements FeatureConfiguration {
             Codec.intRange(0, 100).fieldOf("valid_biome_radius_check").orElse(0).forGetter(config -> config.biomeRadius),
             Codec.intRange(0, 100).fieldOf("structure_set_avoid_radius_check").orElse(0).forGetter(config -> config.structureAvoidRadius),
             ResourceKey.codec(Registry.STRUCTURE_SET_REGISTRY).listOf().fieldOf("structure_set_to_avoid").orElse(new ArrayList<>()).forGetter(config -> config.structureSetToAvoid),
-            Codec.INT.optionalFieldOf("fixed_y_spawn").forGetter(config -> config.fixedYSpawn)
+            Codec.INT.optionalFieldOf("fixed_y_spawn").forGetter(config -> config.fixedYSpawn),
+            Codec.FLOAT.optionalFieldOf("center_terrain_height_weight").forGetter(config -> config.centerTerrainHeightWeight)
     ).apply(instance, RSMonumentConfig::new));
 
     public final String monumentType;
     public final int biomeRadius;
     public final int structureAvoidRadius;
     public final Optional<Integer> fixedYSpawn;
+    public final Optional<Float> centerTerrainHeightWeight;
     public final List<ResourceKey<StructureSet>> structureSetToAvoid;
 
     public RSMonumentConfig(String monumentType,
                         int biomeRadius,
                         int structureAvoidRadius,
                         List<ResourceKey<StructureSet>> structureSetToAvoid,
-                        Optional<Integer> fixedYSpawn)
+                        Optional<Integer> fixedYSpawn,
+                        Optional<Float> centerTerrainHeightWeight)
     {
         this.monumentType = monumentType.toLowerCase(Locale.ROOT);
         this.biomeRadius = biomeRadius;
         this.structureAvoidRadius = structureAvoidRadius;
         this.structureSetToAvoid = structureSetToAvoid;
         this.fixedYSpawn = fixedYSpawn;
+        this.centerTerrainHeightWeight = centerTerrainHeightWeight;
     }
 }
