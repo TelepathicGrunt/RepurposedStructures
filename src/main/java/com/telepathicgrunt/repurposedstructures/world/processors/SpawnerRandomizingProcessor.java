e50f7b545aaf6a116b71e75e3be8099017f23eb2
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2022-06-24 20:52:05 -0400

    upgraded spawner processor

--- src/main/java/com/telepathicgrunt/repurposedstructures/world/processors/SpawnerRandomizingProcessor.java
+++ src/main/java/com/telepathicgrunt/repurposedstructures/world/processors/SpawnerRandomizingProcessor.java
@@ -8,32 +8,73 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.IntTag;
 import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.NbtOps;
+import net.minecraft.nbt.Tag;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.InclusiveRange;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.monster.warden.AngerManagement;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SpawnerBlock;
 import net.minecraft.world.level.levelgen.LegacyRandomSource;
 import net.minecraft.world.level.levelgen.WorldgenRandom;
+import net.minecraft.world.level.levelgen.heightproviders.HeightProvider;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructurePlaceSettings;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProcessor;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProcessorType;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
 import net.minecraftforge.registries.ForgeRegistries;
 
-import java.util.Random;
+import java.util.Optional;
 
 public class SpawnerRandomizingProcessor extends StructureProcessor {
 
     public static final Codec<SpawnerRandomizingProcessor> CODEC = RecordCodecBuilder.create((instance) -> instance.group(
-            ResourceLocation.CODEC.fieldOf("rs_spawner_resourcelocation").forGetter(spawnerRandomizingProcessor -> spawnerRandomizingProcessor.rsSpawnerResourcelocation))
-            .apply(instance, instance.stable(SpawnerRandomizingProcessor::new)));
+            ResourceLocation.CODEC.fieldOf("rs_spawner_resourcelocation").forGetter(spawnerRandomizingProcessor -> spawnerRandomizingProcessor.rsSpawnerResourcelocation),
+            InclusiveRange.INT.optionalFieldOf("valid_block_light_level").forGetter(spawnerRandomizingProcessor -> spawnerRandomizingProcessor.validBlockLightLevel),
+            InclusiveRange.INT.optionalFieldOf("valid_sky_light_level").forGetter(spawnerRandomizingProcessor -> spawnerRandomizingProcessor.validSkyLightLevel),
+            Codec.intRange(0, Integer.MAX_VALUE).fieldOf("delay").orElse(20).forGetter(spawnerRandomizingProcessor -> spawnerRandomizingProcessor.delay),
+            Codec.intRange(0, Integer.MAX_VALUE).fieldOf("max_nearby_entities").orElse(6).forGetter(spawnerRandomizingProcessor -> spawnerRandomizingProcessor.maxNearbyEntities),
+            Codec.intRange(0, Integer.MAX_VALUE).fieldOf("max_spawn_delay").orElse(800).forGetter(spawnerRandomizingProcessor -> spawnerRandomizingProcessor.maxSpawnDelay),
+            Codec.intRange(0, Integer.MAX_VALUE).fieldOf("min_spawn_delay").orElse(200).forGetter(spawnerRandomizingProcessor -> spawnerRandomizingProcessor.minSpawnDelay),
+            Codec.intRange(0, Integer.MAX_VALUE).fieldOf("required_player_range").orElse(16).forGetter(spawnerRandomizingProcessor -> spawnerRandomizingProcessor.requiredPlayerRange),
+            Codec.intRange(0, Integer.MAX_VALUE).fieldOf("spawn_count").orElse(4).forGetter(spawnerRandomizingProcessor -> spawnerRandomizingProcessor.spawnCount),
+            Codec.intRange(0, Integer.MAX_VALUE).fieldOf("spawn_range").orElse(4).forGetter(spawnerRandomizingProcessor -> spawnerRandomizingProcessor.spawnRange)
+    ).apply(instance, instance.stable(SpawnerRandomizingProcessor::new)));
 
     public final ResourceLocation rsSpawnerResourcelocation;
+    public final Optional<InclusiveRange<Integer>> validBlockLightLevel;
+    public final Optional<InclusiveRange<Integer>> validSkyLightLevel;
+    public final int delay;
+    public final int maxNearbyEntities;
+    public final int maxSpawnDelay;
+    public final int minSpawnDelay;
+    public final int requiredPlayerRange;
+    public final int spawnCount;
+    public final int spawnRange;
 
-    private SpawnerRandomizingProcessor(ResourceLocation rsSpawnerResourcelocation) {
+    private SpawnerRandomizingProcessor(ResourceLocation rsSpawnerResourcelocation,
+                                        Optional<InclusiveRange<Integer>> validBlockLightLevel,
+                                        Optional<InclusiveRange<Integer>> validSkyLightLevel,
+                                        int delay,
+                                        int maxNearbyEntities,
+                                        int maxSpawnDelay,
+                                        int minSpawnDelay,
+                                        int requiredPlayerRange,
+                                        int spawnCount,
+                                        int spawnRange) {
         this.rsSpawnerResourcelocation = rsSpawnerResourcelocation;
+        this.validBlockLightLevel = validBlockLightLevel;
+        this.validSkyLightLevel = validSkyLightLevel;
+        this.delay = delay;
+        this.maxNearbyEntities = maxNearbyEntities;
+        this.maxSpawnDelay = maxSpawnDelay;
+        this.minSpawnDelay = minSpawnDelay;
+        this.requiredPlayerRange = requiredPlayerRange;
+        this.spawnCount = spawnCount;
+        this.spawnRange = spawnRange;
     }
 
     @Override
@@ -60,57 +101,58 @@ public class SpawnerRandomizingProcessor extends StructureProcessor {
     private CompoundTag SetMobSpawnerEntity(RandomSource random, CompoundTag nbt) {
         EntityType<?> entity = RepurposedStructures.mobSpawnerManager.getSpawnerMob(rsSpawnerResourcelocation, random);
         if(entity != null) {
-            if(nbt != null) {
-                CompoundTag spawnDataTag = nbt.getCompound("SpawnData");
-                if(spawnDataTag.isEmpty()) {
-                    spawnDataTag = new CompoundTag();
-                    nbt.put("SpawnData", spawnDataTag);
-                }
-                CompoundTag entityTag = nbt.getCompound("entity");
-                if(entityTag.isEmpty()) {
-                    entityTag = new CompoundTag();
-                    spawnDataTag.put("entity", entityTag);
-                }
-                entityTag.putString("id", ForgeRegistries.ENTITIES.getKey(entity).toString());
+            ResourceLocation entityRL = ForgeRegistries.ENTITIES.getKey(entity);
 
-                CompoundTag spawnEntityDataTag = new CompoundTag();
-                spawnEntityDataTag.putString("id", ForgeRegistries.ENTITIES.getKey(entity).toString());
-                CompoundTag spawnPotentialDataEntryTag = new CompoundTag();
-                spawnPotentialDataEntryTag.put("entity", spawnEntityDataTag);
-                CompoundTag spawnPotentialEntryTag = new CompoundTag();
-                spawnPotentialEntryTag.put("data", spawnPotentialDataEntryTag);
-                spawnPotentialEntryTag.put("weight", IntTag.valueOf(1));
-                nbt.put("SpawnPotentials", new ListTag());
-                return nbt;
+            // Set spawn potentials
+            CompoundTag compound = new CompoundTag();
+            compound.putShort("Delay", (short) delay);
+            compound.putShort("MinSpawnDelay", (short) minSpawnDelay);
+            compound.putShort("MaxSpawnDelay", (short) maxSpawnDelay);
+            compound.putShort("SpawnCount", (short) spawnCount);
+            compound.putShort("MaxNearbyEntities", (short) maxNearbyEntities);
+            compound.putShort("RequiredPlayerRange", (short) requiredPlayerRange);
+            compound.putShort("SpawnRange", (short) spawnRange);
+
+            CompoundTag spawnData = new CompoundTag();
+            CompoundTag spawnPotentialData = new CompoundTag();
+            CompoundTag entityData = new CompoundTag();
+            entityData.putString("id", entityRL.toString());
+            spawnPotentialData.put("entity", entityData);
+
+            if (validBlockLightLevel.isPresent() || validSkyLightLevel.isPresent()) {
+                CompoundTag customSpawnRule = new CompoundTag();
+
+                validBlockLightLevel.ifPresent(blockLightLimit -> {
+                    CompoundTag blockLightTag = new CompoundTag();
+                    blockLightTag.putInt("min_inclusive", blockLightLimit.minInclusive());
+                    blockLightTag.putInt("max_inclusive", blockLightLimit.maxInclusive());
+                    customSpawnRule.put("block_light_limit", blockLightTag);
+                });
+                validSkyLightLevel.ifPresent(skyLightLimit -> {
+                    CompoundTag skyLightTag = new CompoundTag();
+                    skyLightTag.putInt("min_inclusive", skyLightLimit.minInclusive());
+                    skyLightTag.putInt("max_exclusive", skyLightLimit.maxInclusive());
+                    customSpawnRule.put("sky_light_limit", skyLightTag);
+                });
+
+                spawnPotentialData.put("custom_spawn_rules", customSpawnRule);
+                spawnData.put("custom_spawn_rules", customSpawnRule);
             }
-            else {
-                CompoundTag compound = new CompoundTag();
-                compound.putShort("Delay", (short) 20);
-                compound.putShort("MinSpawnDelay", (short) 200);
-                compound.putShort("MaxSpawnDelay", (short) 800);
-                compound.putShort("SpawnCount", (short) 4);
-                compound.putShort("MaxNearbyEntities", (short) 6);
-                compound.putShort("RequiredPlayerRange", (short) 16);
-                compound.putShort("SpawnRange", (short) 4);
 
-                CompoundTag spawnDataEntity = new CompoundTag();
-                spawnDataEntity.putString("id", ForgeRegistries.ENTITIES.getKey(entity).toString());
-                CompoundTag spawnData = new CompoundTag();
-                spawnData.put("entity", spawnDataEntity);
-                compound.put("SpawnData", spawnData);
+            CompoundTag listEntry = new CompoundTag();
+            listEntry.put("data", spawnPotentialData);
+            listEntry.putInt("weight", 1);
+            ListTag listTag = new ListTag();
+            listTag.add(listEntry);
+            compound.put("SpawnPotentials", listTag);
 
-                CompoundTag entityData = new CompoundTag();
-                entityData.putString("id", ForgeRegistries.ENTITIES.getKey(entity).toString());
 
-                CompoundTag spawnPotentialData = new CompoundTag();
-                spawnPotentialData.put("entity", entityData);
-                CompoundTag listEntry = new CompoundTag();
-                listEntry.put("data", spawnPotentialData);
-                listEntry.putInt("weight", 1);
-                compound.put("SpawnPotentials", new ListTag());
+            CompoundTag entityEntry = new CompoundTag();
+            entityEntry.putString("id", entityRL.toString());
+            spawnData.put("entity", entityEntry);
+            compound.put("SpawnData", spawnData);
 
-                return compound;
-            }
+            return compound;
         }
 
         return null;
