32333cee45ac1c7b085a5a7f0ea412224f70d875
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2023-04-20 17:04:11 -0400

    silence logspam with yung api

--- src/main/java/com/telepathicgrunt/repurposedstructures/mixin/structures/StructurePoolMixin.java
+++ src/main/java/com/telepathicgrunt/repurposedstructures/mixin/structures/StructurePoolMixin.java
@@ -1,11 +1,14 @@
 package com.telepathicgrunt.repurposedstructures.mixin.structures;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.mojang.serialization.Codec;
 import net.minecraft.world.level.levelgen.structure.pools.StructureTemplatePool;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
-@SuppressWarnings("UnresolvedMixinReference")
 @Mixin(StructureTemplatePool.class)
 public class StructurePoolMixin {
 
@@ -14,14 +17,13 @@ public class StructurePoolMixin {
      * @author - TelepathicGrunt
      * @return - The higher weight that is a more reasonable limit.
      */
-    @ModifyConstant(
+    @WrapOperation(
             method = "m_dgkaflam",
-            constant = @Constant(intValue = 150),
-            remap = false,
+            at = @At(value = "INVOKE", target = "Lcom/mojang/serialization/Codec;intRange(II)Lcom/mojang/serialization/Codec;"),
             require = 0
     )
-    private static int repurposedstructures_increaseWeightLimitDev(int constant) {
-        return 5000;
+    private static Codec<Integer> repurposedstructures_increaseWeightLimitDev(int minRange, int maxRange, Operation<Codec<Integer>> original) {
+        return original.call(minRange, 5000);
     }
 
     /**
@@ -29,13 +31,12 @@ public class StructurePoolMixin {
      * @author - TelepathicGrunt
      * @return - The higher weight that is a more reasonable limit.
      */
-    @ModifyConstant(
+    @WrapOperation(
             method = "method_28886",
-            constant = @Constant(intValue = 150),
-            remap = false,
+            at = @At(value = "INVOKE", target = "Lcom/mojang/serialization/Codec;intRange(II)Lcom/mojang/serialization/Codec;"),
             require = 0
     )
-    private static int repurposedstructures_increaseWeightLimitProd(int constant) {
-        return 5000;
+    private static Codec<Integer> repurposedstructures_increaseWeightLimitProd(int minRange, int maxRange, Operation<Codec<Integer>> original) {
+        return original.call(minRange, 5000);
     }
 }
