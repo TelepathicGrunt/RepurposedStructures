30aadbcfd537697a2b51bb2a61f145d859cda32b
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2024-08-18 07:06:54 -0400

    turned off basalt logging

--- src/main/java/com/telepathicgrunt/repurposedstructures/mixin/features/NoBasaltColumnsInStructuresMixin.java
+++ src/main/java/com/telepathicgrunt/repurposedstructures/mixin/features/NoBasaltColumnsInStructuresMixin.java
@@ -34,7 +34,7 @@ public class NoBasaltColumnsInStructuresMixin {
 
         SectionPos sectionPos = SectionPos.of(mutableBlockPos);
         if (!levelAccessor.getChunk(sectionPos.x(), sectionPos.z()).getStatus().isOrAfter(ChunkStatus.STRUCTURE_REFERENCES)) {
-            RepurposedStructures.LOGGER.warn("Repurposed Structures: Detected a mod with a broken basalt columns configuredfeature that is trying to place blocks outside the 3x3 safe chunk area for features. Find the broken mod and report to them to fix the placement of their basalt columns feature.");
+            //RepurposedStructures.LOGGER.warn("Repurposed Structures: Detected a mod with a broken basalt columns configuredfeature that is trying to place blocks outside the 3x3 safe chunk area for features. Find the broken mod and report to them to fix the placement of their basalt columns feature.");
             return;
         }
 
